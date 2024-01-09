e834c592692780245569e5fc3b08ffd6840e641d
Jon Thompson <jon@jont.software>
2024-01-09 09:40:38 +0000

    adds runtime plugin

--- apps/host/next.config.js
+++ apps/host/next.config.js
@@ -1,18 +1,6 @@
-//@ts-check
-
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 const { NextFederationPlugin } = require('@module-federation/nextjs-mf');
-const { createDelegatedModule } = require('@module-federation/utilities');
 const { composePlugins, withNx } = require('@nx/next');
-
-const remotes = (/** @type boolean */ isServer) => {
-  const location = isServer ? 'ssr' : 'chunks';
-  return {
-    shop: createDelegatedModule(require.resolve('./remote-delegate.js'), {
-      remote: `shop@http://localhost:4201/_next/static/${location}/remoteEntry.js`,
-    }),
-  };
-};
 
 /**
  * @type {import('@nx/next/plugins/with-nx').WithNxOptions}
@@ -35,11 +23,16 @@ const nextConfig = {
       ...config.resolve,
       fallback: { fs: false, net: false, tls: false },
     };
+
+    const location = isServer ? 'ssr' : 'chunks';
+
     config.plugins.push(
       new NextFederationPlugin({
         name: 'host',
         filename: 'static/chunks/remoteEntry.js',
-        remotes: remotes(isServer),
+        remotes: {
+          shop: `shop@http://localhost:4201/_next/static/${location}/remoteEntry.js`,
+        },
         shared: {
           '@emotion/react': {
             singleton: true,
@@ -48,7 +41,7 @@ const nextConfig = {
             singleton: true,
           },
         },
-        extraOptions: {},
+        runtimePlugins: [require.resolve('./runtime-plugin.js')],
       })
     );
 
